b1f33148abab59c9c025a723abf7bee05906e0e4
Juan <jguardado85@gmail.com>
2020-11-17 09:33:16 -0800

    clean up

--- test/reverseStringTest.js
+++ test/reverseStringTest.js
@@ -1,19 +1,16 @@
 /* 
 example Test
  
-to run the test, make sure you've installed node modules and run the following command.
+to run the test, make sure you've installed node modules and run the following command:
 
-
+mocha
 
 */
-var { assert } = require('chai');
+var { expect } = require('chai');
 var reverseString = require('../reverseString');
 
 describe('reverseString', () => {
   it('should reverse a string', () => {
-    const input = 'sup'
-    const output = 'pus'
-
-    assert.equal(reverseString(input), output)
+    expect(reverseString('hello world')).to.equal('dlrow olleh')
   })
 })
